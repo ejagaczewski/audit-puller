441fa9d3a800f5997f3c9e63d56ded48070cc219
ejagaczewski <66079979+ejagaczewski@users.noreply.github.com>
2020-05-29 14:12:10 -0500

    Create audit-puller.py

---
+++ audit-puller.py
@@ -0,0 +1,54 @@
+import requests, json, csv, time, os
+import pandas as pd
+
+
+#Prisma Cloud Access Keys
+accessKey = ""
+secretKey = ""
+
+#Specify correct API address
+apiUrl = "https://api3.prismacloud.io/audit/redlock"
+
+#Time unit for audit logs https://api.docs.prismacloud.io/reference#rl-audit-logs
+timeUnit = "day"
+#Amount of time unit
+timeAmount = "120"
+
+#Enter correct URL for tenant
+loginUrl = "https://api3.prismacloud.io/login"
+
+loginPayload = '{"username" : "' + accessKey + '", "password" : "' + secretKey + '"}'
+
+headers = {
+    'accept': "application/json; charset=UTF-8",
+    'content-type': "application/json; charset=UTF-8"
+    }
+
+#Get JWT Token for login
+loginResponse = requests.request("POST", loginUrl, data=loginPayload, headers=headers, verify=False)
+jsonResponse = loginResponse.json()
+
+#Extract token from json
+token = jsonResponse['token']
+
+#Pull audit logs
+
+
+querystring = {"timeType":"relative","timeAmount": timeAmount,"timeUnit": timeUnit}
+
+headers = {'accept': "application/json; charset=UTF-8",'x-redlock-auth': token}
+
+auditLogs = requests.request("GET", apiUrl, headers=headers, params=querystring, verify=False)
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
+print(timestr)
+
+#Output to file
+f = open(timestr + '.txt', "w")
+f.write(auditLogs.text)
+#Convert to CSV
+df = pd.read_json (timestr + '.txt')
+df.to_csv ('logs/' + timestr + '.csv', index = None)
+#Cleanup
+os.remove(timestr + '.txt')
+print("Pulled audit logs sucessfully and wrote them to " + timestr +".csv")
