a1551c89b7f32c0faff4fe062b388cd86e0a5de3
ejagaczewski <66079979+ejagaczewski@users.noreply.github.com>
2020-05-30 20:01:41 -0500

    Update audit-puller.py
    Added S3 Copy

--- audit-puller.py
+++ audit-puller.py
@@ -1,25 +1,29 @@
-import requests, json, csv, time, os
+import requests, json, time, os, boto3
 import pandas as pd
 
 #Get Access keys from environment variables PC_ACCESS_KEY and PC_SECRET_KEY
+
 accessKey = os.environ.get("PC_ACCESS_KEY")
 secretKey = os.environ.get("PC_SECRET_KEY")
 
-#Hardcode Prisma Cloud Access Keys *NOT RECOMMENDED*
-#accessKey = ""
-#secretKey = ""
+#Hard-code Prisma Cloud Access Keys *NOT RECOMMENDED*
+accessKey = ""
+secretKey = ""
+
+#Bucket for optional S3 upload
+#bucketName = ""
 
 #Specify correct API address
 apiUrl = "https://api3.prismacloud.io/audit/redlock"
-#Specify login URL for PrismaCloud
-loginUrl = "https://api3.prismacloud.io/login"
 
 #Time unit for audit logs https://api.docs.prismacloud.io/reference#rl-audit-logs
 timeUnit = "day"
 #Amount of time unit
 timeAmount = "120"
 
-#Set login payload
+#Enter URL for tenant
+loginUrl = "https://api3.prismacloud.io/login"
+
 loginPayload = '{"username" : "' + accessKey + '", "password" : "' + secretKey + '"}'
 
 headers = {
@@ -28,20 +32,22 @@ headers = {
     }
 
 #Get JWT Token for login
-loginResponse = requests.request("POST", loginUrl, data=loginPayload, headers=headers)
+loginResponse = requests.request("POST", loginUrl, data=loginPayload, headers=headers, verify=False)
 jsonResponse = loginResponse.json()
-
-#Extract token from JSON
+print(loginResponse.text)
+#Extract token from json
 token = jsonResponse['token']
 
 #Pull audit logs
+
 querystring = {"timeType":"relative","timeAmount": timeAmount,"timeUnit": timeUnit}
 
 headers = {'accept': "application/json; charset=UTF-8",'x-redlock-auth': token}
 
-auditLogs = requests.request("GET", apiUrl, headers=headers, params=querystring)
+auditLogs = requests.request("GET", apiUrl, headers=headers, params=querystring, verify=False)
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
+print(timestr)
 
 #Output to file
 f = open(timestr + '.txt', "w")
@@ -52,3 +58,8 @@ df.to_csv ('logs/' + timestr + '.csv', index = None)
 #Cleanup
 os.remove(timestr + '.txt')
 print("Pulled audit logs sucessfully and wrote them to " + timestr +".csv")
+
+#print("Writing files to S3...")
+#Copy CSV to S3 bucket
+#s3 = boto3.client('s3')
+#s3.upload_file('logs/' + timestr +".csv", bucketName, timestr +".csv")
